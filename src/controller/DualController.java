660edfc5b22ba22f502cfe411a30440a39d4a20b
Ryan <jlkfop@gmail.com>
2019-05-13 08:00:30 -0600

    I added a main method and a start method that do nothing

---
+++ src/controller/DualController.java
@@ -0,0 +1,9 @@
+package controller;
+
+public class DualController 
+{
+	public void start()
+	{
+		
+	}
+}
---
+++ src/controller/DualRunner.java
@@ -0,0 +1,10 @@
+package controller;
+
+public class DualRunner 
+{
+	public static void main(String args[])
+	{
+		DualController app = new DualController();
+		app.start();
+	}
+}

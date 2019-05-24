24e685690978b1f9977f9646b428cc8872226acd
rphi8796 <jlkfop@gmail.com>
2019-05-23 20:37:28 -0600

    updated gitignore

--- src/controller/IOController.java
+++ src/controller/IOController.java
@@ -39,7 +39,7 @@ public class IOController
 	{
 		try
 		{
-			File soundFile = new File("/Users/rphi8796/Documents/Java/DualProject/src/sounds/" + fileName + ".wav");
+			File soundFile = new File("src/sounds/" + fileName + ".wav");
 			AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
 			Clip comeOn = AudioSystem.getClip();
 			comeOn.open(audioIn);
